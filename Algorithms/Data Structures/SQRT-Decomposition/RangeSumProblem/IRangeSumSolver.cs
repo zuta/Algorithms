77502486eac540069563c34cb7c799c80917b935
Taras Zubyk <taras.zubyk@i.ua>
2013-03-31 00:28:15 +0200

    Remove unnecessary using-statements

--- Algorithms/Data Structures/SQRT-Decomposition/RangeSumProblem/IRangeSumSolver.cs
+++ Algorithms/Data Structures/SQRT-Decomposition/RangeSumProblem/IRangeSumSolver.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructures.SqrtDecomposition.RangeSumProblem
 {
     public interface IRangeSumSolver
     {
-
         List<int> Solve(int[] array, List<Request> requests);
-
     }
 }
--- Algorithms/Sorting/HeapSortLibrary/HeapSort.cs
+++ Algorithms/Sorting/HeapSortLibrary/HeapSort.cs
@@ -3,8 +3,6 @@ using BinaryHeapLibrary;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HeapSortLibrary
 {
